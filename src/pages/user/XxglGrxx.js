085e5be0a83fb9a67874b00c2c0c3062d9377f3b
KoonChen <zpkoon@gmail.com>
2018-11-25 12:15:34 +0800

    🚧 do sth

--- src/pages/user/XxglGrxx.js
+++ src/pages/user/XxglGrxx.js
@@ -1,11 +1,64 @@
 //【信息管理-个人信息】页面
 import React from 'react';
+import axios from 'axios';
+import {test_url} from '../../util/Request';
+import { Skeleton, Switch, Card, Icon, Avatar } from 'antd';
+
+const { Meta } = Card;
 
 class UserXxglGrxx extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true, state_: '0', email: 'test', des: 'test' };
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  onChange = (checked) => {
+    this.setState({ loading: !checked });
+  }
+
+  componentDidMount() {
+    axios.get(test_url + 'User/' + 'user1@email.com')
+      .then(function(response){
+        if (response.status == 200) {
+          this.setState({state_: response.data.state + ""});
+          this.setState({email: response.data.email});
+          this.setState({accountBalance: response.data.accountBalance});
+
+          this.onChange(this.state);
+        }
+      }.bind(this))
+  }
+
     render(){
+      const { loading } = this.state;
       return (
-        <div>
-            陈泽平开始工作啦
+        <div style={{display: 'flex', justifyContent:'space-between', width:'1000px' }}>
+          {/* <Switch checked={!loading} onChange={this.onChange} /> */}
+          <Card style={{ width: 300, marginTop: 16 }} loading={loading}>
+            <Meta
+              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+              title="State"
+              description={this.state.state_}
+            />
+          </Card>
+
+          <Card style={{ width: 300, marginTop: 16 }} loading={loading}>
+            <Meta
+              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+              title="Email"
+              description={this.state.email}
+            />
+          </Card>
+
+          <Card style={{ width: 300, marginTop: 16 }} loading={loading}>
+            <Meta
+              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+              title="AccountBalance"
+              description={this.state.accountBalance}
+            />
+          </Card>
+
         </div>
       );
     }
--- src/util/Request.js
+++ src/util/Request.js
@@ -1,2 +1,4 @@
 const axios = require('axios');
 axios.defaults.baseURL = 'http://api.example.com';
+
+export const test_url = 'http://47.98.41.183:3000/api/';
