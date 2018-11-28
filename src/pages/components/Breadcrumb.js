eddbfa46107d90ef4ba0693af9a280971f9af464
chenzhou <951368548@qq.com>
2018-11-28 22:11:06 +0800

    20181128

--- src/pages/components/Breadcrumb.js
+++ src/pages/components/Breadcrumb.js
@@ -9,7 +9,6 @@ class MyBreadcrumb extends React.Component {
             <Breadcrumb.Item>List</Breadcrumb.Item>
             <Breadcrumb.Item>App</Breadcrumb.Item>
         </Breadcrumb>
-        <image src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1543320677076&di=3979c29a323c25e53486523a88e04bda&imgtype=0&src=http%3A%2F%2Fpic5.photophoto.cn%2F20071231%2F0017029450803123_b.jpg"  />
 
       </div>
     );
